da302944f0aa69d0e5def60a2fd0ed887d4521e6
Len Adler <lmadler72@eclipso.de>
2024-07-23 19:33:45 +0200

    widgets is needed for test because else an undefined reference to QApplication appears

--- Wavedit/src/WavFourier.h
+++ Wavedit/src/WavFourier.h
@@ -3,7 +3,6 @@
 
 #include <QObject>
 #include <QList>
-#include <QFileDialog>
 #include <QString>
 #include <QTime>
 
--- test/moc_WavFourier.cpp
+++ test/moc_WavFourier.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_WavFourier_t {
-    QByteArrayData data[5];
-    char stringdata0[63];
+    QByteArrayData data[4];
+    char stringdata0[36];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,12 +35,10 @@ static const qt_meta_stringdata_WavFourier_t qt_meta_stringdata_WavFourier = {
 QT_MOC_LITERAL(0, 0, 10), // "WavFourier"
 QT_MOC_LITERAL(1, 11, 7), // "gotData"
 QT_MOC_LITERAL(2, 19, 0), // ""
-QT_MOC_LITERAL(3, 20, 15), // "failedToGetData"
-QT_MOC_LITERAL(4, 36, 26) // "handleOpenFileDialogButton"
+QT_MOC_LITERAL(3, 20, 15) // "failedToGetData"
 
     },
-    "WavFourier\0gotData\0\0failedToGetData\0"
-    "handleOpenFileDialogButton"
+    "WavFourier\0gotData\0\0failedToGetData"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +48,7 @@ static const uint qt_meta_data_WavFourier[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,17 +56,11 @@ static const uint qt_meta_data_WavFourier[] = {
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x06 /* Public */,
-       3,    0,   30,    2, 0x06 /* Public */,
-
- // slots: name, argc, parameters, tag, flags
-       4,    0,   31,    2, 0x0a /* Public */,
+       1,    0,   24,    2, 0x06 /* Public */,
+       3,    0,   25,    2, 0x06 /* Public */,
 
  // signals: parameters
     QMetaType::Void,
-    QMetaType::Void,
-
- // slots: parameters
     QMetaType::Void,
 
        0        // eod
@@ -82,7 +74,6 @@ void WavFourier::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->gotData(); break;
         case 1: _t->failedToGetData(); break;
-        case 2: _t->handleOpenFileDialogButton(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -134,13 +125,13 @@ int WavFourier::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 2)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 2;
     }
     return _id;
 }
