5e3641792f5875d6def37c3b4cafdd38d96ef60f
Len Adler <lmadler72@eclipso.de>
2024-07-24 19:17:12 +0200

    Test WavFourier::Freq

--- test/src/TestWavFourier.cpp
+++ test/src/TestWavFourier.cpp
@@ -43,37 +43,59 @@ void TestWavFourier::init()
 
 void TestWavFourier::testGetStuetzstellen_data()
 {
-	QTest::addColumn<QVector<double>>("data");
+	QTest::addColumn<int>("size");
 	QTest::addColumn<QVector<double>>("stuetzstellen");
 
 	// data.size, correct result are stored in table
-	QTest::newRow("empty data") << QVector<double>() << QVector<double>({});
-	QTest::newRow("data.size() = 4") << QVector<double>(4) << QVector<double>({0, M_PI/2.0, M_PI, 3.0*M_PI/2.0});
-	QTest::newRow("data.size() = 7") << QVector<double>(7) << QVector<double>(
+	QTest::newRow("empty data") << 0 << QVector<double>({});
+	QTest::newRow("data.size() = 4") << 4 << QVector<double>({0, M_PI/2.0, M_PI, 3.0*M_PI/2.0});
+	QTest::newRow("data.size() = 7") << 7 << QVector<double>(
 		{0, 2.0*M_PI/7.0, 4.0*M_PI/7.0, 6.0*M_PI/7.0, 8.0*M_PI/7.0, 10.0*M_PI/7.0, 12.0*M_PI/7.0}
 	);
 }
 
 void TestWavFourier::testGetStuetzstellen()
 {
-	QFETCH(QVector<double>, data);
+	QFETCH(int, size);
 	QFETCH(QVector<double>, stuetzstellen);
-	QCOMPARE(wavfourier.getStuetzstellen(data.size()), stuetzstellen);
+
+	QCOMPARE(wavfourier.getStuetzstellen(size), stuetzstellen);
 }
 
 
 void TestWavFourier::testFreq_data()
 {
-	QTest::addColumn<QVector<double>>("data");
-	QTest::addColumn<QVector<double>>("sample_spacing");
-	QTest::addColumn<QVector<double>>("frequency bins");
+	QTest::addColumn<int>("data_size");
+	QTest::addColumn<double>("sample_rate");
+	QTest::addColumn<QVector<double>>("frequency_bins");
 
+	QTest::newRow("empty data, default sample_rate") << 0 << 1.0 << QVector<double>({});
+	QTest::newRow("empty data, sample_rate 10 Hz") << 0 << 10.0 << QVector<double>({});
+
+	QTest::newRow("data.size() = 4, default sample_rate") << 4 << 1.0 << QVector<double>(
+		{0, 1.0/4.0}
+	);
+
+	QTest::newRow("data.size() = 4, , sample_rate 10 Hz") << 4 << 10.0 << QVector<double>(
+		{0, 5.0/2.0}
+	);
+
+	QTest::newRow("data.size() = 7, default sample_rate") << 7 << 1.0 << QVector<double>(
+		{0, 1.0/7.0, 2.0/7.0}
+	);
+	QTest::newRow("data.size() = 7, sample_rate 10 Hz") << 7 << 10.0 << QVector<double>(
+		{0, 10.0/7.0, 20.0/7.0}
+	);
 
 }
 
 void TestWavFourier::testFreq()
 {
+	QFETCH(int, data_size);
+	QFETCH(double, sample_rate);
+	QFETCH(QVector<double>, frequency_bins);
 
+	QCOMPARE(wavfourier.Freq(data_size, sample_rate), frequency_bins);
 }
 
 void TestWavFourier::testDFT()
